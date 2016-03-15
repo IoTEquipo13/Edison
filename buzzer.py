6a561eb0a531336e2287b700afc3727217e74d6f
killuazoldyck90 <a_h5n1@live.com>
2016-03-15 00:53:24 +0000

    added proper led functionality on pin D4

--- buzzer.py
+++ buzzer.py
@@ -6,7 +6,6 @@ import pyupm_i2clcd as lcd
 import sys
 import random
 
-
 counter = 0
 
 # Create the button object using GPIO pin 2
@@ -26,8 +25,6 @@ chords = [upmBuzzer.DO, upmBuzzer.RE, upmBuzzer.MI, upmBuzzer.FA,
 #Crete the lcd object
 lcdDisplay = lcd.Jhd1313m1(0, 0x3E, 0x62)
 lcdDisplay.setColor(0, 0, 0)
-
-
 lcdDisplay.write(str(counter))
 
 # Read the input and print, waiting one second between readings
@@ -45,14 +42,15 @@ while 1:
       # play each note for one second
       print ("Boton presionado")
       buzzer.playSound(chords[chord_ind], 1000000)
-      time.sleep(0.0001)
+      time.sleep(0.1)
       led.off()
 
 # Delete the buzzer object
 del buzzer
 
 # Delete the button object
-
 del button
 
+# Delete the led object
+del led
 
