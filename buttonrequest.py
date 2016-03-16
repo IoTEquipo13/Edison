46c085557ec034e6949d072235fef9887c88e4b5
killuazoldyck90 <a_h5n1@live.com>
2016-03-16 18:20:54 +0000

    send request added

--- buttonrequest.py
+++ buttonrequest.py
@@ -5,11 +5,15 @@ button = mraa.Gpio(2)
 button.dir(mraa.DIR_IN)
 
 while 1:
-	placa = 'Expreso'
+	placa = 'KL54A7860'
 	if(button.read() != 0):
-		response = requests.get('https://greenheadapi.azurewebsites.net/api/user/getUser/' + placa)
-		assert response.status_code == 200
-		print response.json();
+		response = requests.get('https://greenheadapi.azurewebsites.net/api/place/getzone/' + placa)
+		#assert response.status_code == 200
+		#print response.status_code
+		if(response.status_code == 500):
+			continue
+		else:
+			print response.json();
 		"""
 		for repo in response.json():
 			print '[{}]
