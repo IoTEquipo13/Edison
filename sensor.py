593ed8369eb7588ba35e44e497503ed7a459e90f
Bryan Arroyo <arroyo_bryan@hotmail.com>
2016-03-17 22:31:34 -0600

    sensor ..

--- sensor.py
+++ sensor.py
@@ -22,28 +22,28 @@ touchPin.dir(mraa.DIR_IN)
 # display - lcd
 lcdDisplay = lcd.Jhd1313m1(0, 0x3E, 0x62)
  
-#while 1:
+while 1:
 
-while touchPin.read() == 1:   
+    while touchPin.read() == 1:   
 
-    # turn led on
-    ledPin.write(1)
-   
-    # read pot/print/convert to string/display on lcd
-    potVal = float(pot.read())
-    #print potVal   
-    #potStr = str(potVal)
-    if potVal:
+        # turn led on
+        ledPin.write(1)
+       
+        # read pot/print/convert to string/display on lcd
+        potVal = float(pot.read())
+        #print potVal   
+        #potStr = str(potVal)
+        if potVal:
+            lcdDisplay.setCursor(0, 0)
+            lcdDisplay.setColor(255, 0, 0)
+            lcdDisplay.write("No parking space")
+
         lcdDisplay.setCursor(0, 0)
-        lcdDisplay.setColor(255, 0, 0)
-        lcdDisplay.write("No parking space")
-
-    lcdDisplay.setCursor(0, 0)
-    lcdDisplay.setColor(0, 255, 0)
-    lcdDisplay.write("Available parking spaces") 
+        lcdDisplay.setColor(0, 255, 0)
+        lcdDisplay.write("Available parking spaces") 
 
 
-    time.sleep(1)
+        time.sleep(1)
 
-# turn led off
-ledPin.write(0)
+    # turn led off
+    ledPin.write(0)
