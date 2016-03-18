8b6ec61d565e65afc26a6ce6d9735e62c8168fa0
Eric Reyes <eric.reyes@datarobot.com>
2016-03-18 12:45:09 -0600

    Send another image. PLATE4 ftw

--- send_image.py
+++ send_image.py
@@ -1,10 +1,12 @@
+
 import requests
 
-files = {'file': open('plate.jpg', 'rb')}
+files = {'file': open('plate4.jpg', 'rb')}
+
+
 response = requests.post('http://189.234.140.164:8080/send-image', files=files)
 
 
 assert response.status_code == 200
 
 print response.json()
-#open
