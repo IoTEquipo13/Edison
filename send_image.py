672da97c59e61ffbd20b88fe37ba4be7c4bd0b08
Eric Reyes <eric.reyes@datarobot.com>
2016-03-17 18:51:02 -0600

    Send image over requests

--- send_image.py
+++ send_image.py
@@ -1,10 +1,8 @@
 import requests
 
+files = {'file': open('plate.jpg', 'rb')}
+response = requests.post('http://189.234.140.164:8080/send-image', files=files)
 
-print "ANTES DEL GET"
-response = requests.get('http://189.234.140.164:80')
-
-print "despues del get"
 
 assert response.status_code == 200
 
